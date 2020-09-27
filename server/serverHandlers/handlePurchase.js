813e879d1b62e3ae63189ce5cbbf907d2eaade41
Etienne Giroux <61565800+eggiroux@users.noreply.github.com>
2020-09-27 10:35:42 -0400

    actually fixed the return on purchase handler

--- server/serverHandlers/handlePurchase.js
+++ server/serverHandlers/handlePurchase.js
@@ -4,12 +4,11 @@ const { v4: uuidv4 } = require("uuid");
 const handlePurchase = (req, res) => {
   const purchaseArray = req.body;
   let uuid = uuidv4();
-
   newOrderId = uuid.slice(0, 13);
 
   let newOrder = {};
-  let error = false;
 
+  let error = false;
   purchaseArray.forEach((element) => {
     const [id] = Object.keys(element);
     const [quantity] = Object.values(element);
@@ -18,27 +17,25 @@ const handlePurchase = (req, res) => {
       if (element._id === Number(id)) {
         if (element.numInStock < quantity) {
           error = true;
-          res.status(400).send({
-            error: `Not enough items in stock`,
-          });
+          return;
         }
-      }
-
-      if (!error && element._id === Number(id)) {
         element.numInStock -= quantity;
         newOrder[id] = quantity;
       }
     });
   });
-
-  if (!error) {
-    orders[newOrderId] = newOrder;
-
-    const delay = Math.random() * 1250 + 500;
-    setTimeout(() => {
-      res.status(200).send({ status: "success", orderId: newOrderId });
-    }, delay);
+  if (error) {
+    //console.log("error true, early return");
+    return res.status(400).send({
+      error: `Not enough items in stock`,
+    });
   }
+
+  orders[newOrderId] = newOrder;
+  const delay = Math.random() * 1250 + 500;
+  setTimeout(() => {
+    res.status(200).send({ status: "success", orderId: newOrderId });
+  }, delay);
 };
 
 module.exports = {
